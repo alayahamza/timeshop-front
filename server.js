a82fd65511f979cc1edf422c37e8e04f9f0ec093
alayahamza <alaya.hamza@gmail.com>
2020-05-14 19:44:57 +0200

    Fixes production deployment

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/time-shop-front'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/frontend/'}),
+  res.sendFile('index.html', {root: 'dist/time-shop-front/'}),
 );
 
 app.listen(process.env.PORT || 8080);
